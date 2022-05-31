2106f599358ae346716db9a7c5f8b45636d5b16d
Kevin Christensen <KevinIsAProfessional@gmail.com>
2022-05-31 16:56:29 -0600

    refactor: Change App from a class to a function
    Changing App from a class to a function allows the use of hooks and
    makes the code more streamlined.
    
    Removed backup App.js file, I wasn't using it anymore

--- src/App.js
+++ src/App.js
@@ -1,34 +1,29 @@
 import { Amplify, API, graphqlOperation } from 'aws-amplify';
 import { Authenticator } from '@aws-amplify/ui-react';
-import { Component } from 'react';
+import { Component, useState } from 'react';
 import '@aws-amplify/ui-react/styles.css';
+
 
 import { Welcome, Clock, Staple } from './components.js';
 
 import awsExports from './aws-exports';
 Amplify.configure(awsExports);
 
+function App() {
+  const [count, setCount ] = useState(0);
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { count: 0 }
-  }
-
-  render() {
-    return (
+  return (
       <Authenticator>
-        {({ signOut, user }) => (
+        {({ signOut, user,  }) => (
           <main>
             <button onClick={signOut}>Sign out</button>
             <Welcome name={user.username} />
             <Clock />
-            <Staple count={this.state.count} />
+            <Staple count={count} />
           </main>
         )}
       </Authenticator>
-    );
-  }
+  );
 }
 
 export default App;
